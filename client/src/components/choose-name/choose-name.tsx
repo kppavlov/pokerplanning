02a781920d47d6a4d2d83ee0ea4d1b2c2a50e5e0
Kaloyan <kaloyan@DESKTOP-K4RNKM5.localdomain>
2023-05-28 21:17:59 +0300

    fix build error

--- client/src/components/choose-name/choose-name.tsx
+++ client/src/components/choose-name/choose-name.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const ChooseName = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
