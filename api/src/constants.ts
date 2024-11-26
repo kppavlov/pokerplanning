c4d3c102e2d72cd5819fe453793737426368fce3
Kaloyan <kaloyannn124@gmail.com>
2024-11-26 17:51:04 +0200

    move util to utils

--- api/src/constants.ts
+++ api/src/constants.ts
@@ -1,60 +1,15 @@
 import {
   checkIfNameIsTaken,
   checkIfRoomExists,
+  newInterval,
   removeUserFromRoom,
   resetUserVote,
+  timersMap,
   updateModeratorState,
   updateUserVote,
 } from "./utils";
-import { EventListenerCallback } from "./types";
 import { joinRoomHandler } from "./socket-listeners-handlers";
-import { Server } from "socket.io";
-
-const timersMap = new Map();
-
-const newInterval = ({
-  io,
-  roomId,
-  seconds,
-  minutes,
-}: {
-  io: Server;
-  roomId: string;
-  seconds: number;
-  minutes: number;
-}) => {
-  let newSec = seconds;
-  let newMin = minutes;
-
-  const interval = setInterval(() => {
-    if (newSec !== 0) {
-      --newSec;
-    }
-
-    if (newMin === 0 && newSec === 0) {
-      io.to(roomId).emit("timer-tick", { seconds: newSec, minutes: newMin });
-      io.to(roomId).emit("timer-stop");
-      io.to(roomId).emit("reveal-result");
-
-      clearInterval(timersMap.get(roomId));
-      timersMap.delete(roomId);
-      console.log(interval, timersMap);
-      return;
-    }
-
-    if (newSec === 0) {
-      newSec = 59;
-      newMin = newMin === 0 ? 0 : --newMin;
-
-      io.to(roomId).emit("timer-tick", { seconds: newSec, minutes: newMin });
-
-      return;
-    }
-    io.to(roomId).emit("timer-tick", { seconds: newSec, minutes: newMin });
-  }, 1000);
-
-  timersMap.set(roomId, interval);
-};
+import { EventListenerCallback } from "./types";
 
 export const socketListeners: Record<string, EventListenerCallback> = {
   "join-room": joinRoomHandler,
--- api/src/utils.ts
+++ api/src/utils.ts
@@ -4,6 +4,7 @@ import {
   RoomsType,
   UsersInTheRoomProps,
 } from "./types";
+import { Server } from "socket.io";
 
 export const checkIfRoomExists = (roomName: string, roomsMap: RoomsType) =>
   roomsMap.has(roomName);
@@ -143,4 +144,50 @@ export const updateModeratorState = ({
 
     return updatedRoomUsers;
   }
+};
+
+export const timersMap = new Map();
+
+export const newInterval = ({
+  io,
+  roomId,
+  seconds,
+  minutes,
+}: {
+  io: Server;
+  roomId: string;
+  seconds: number;
+  minutes: number;
+}) => {
+  let newSec = seconds;
+  let newMin = minutes;
+
+  const interval = setInterval(() => {
+    if (newSec !== 0) {
+      --newSec;
+    }
+
+    if (newMin === 0 && newSec === 0) {
+      io.to(roomId).emit("timer-tick", { seconds: newSec, minutes: newMin });
+      io.to(roomId).emit("timer-stop");
+      io.to(roomId).emit("reveal-result");
+
+      clearInterval(timersMap.get(roomId));
+      timersMap.delete(roomId);
+      console.log(interval, timersMap);
+      return;
+    }
+
+    if (newSec === 0) {
+      newSec = 59;
+      newMin = newMin === 0 ? 0 : --newMin;
+
+      io.to(roomId).emit("timer-tick", { seconds: newSec, minutes: newMin });
+
+      return;
+    }
+    io.to(roomId).emit("timer-tick", { seconds: newSec, minutes: newMin });
+  }, 1000);
+
+  timersMap.set(roomId, interval);
 };
